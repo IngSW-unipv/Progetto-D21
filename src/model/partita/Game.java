52d719bfdfb008caa1a28f4a3ffb4cd96cc23b3a
FlavioBobba99 <80460277+FlavioBobba99@users.noreply.github.com>
2021-04-24 12:11:13 +0200

    aggiunta dettagli

--- src/model/partita/Game.java
+++ src/model/partita/Game.java
@@ -1,10 +1,29 @@
 package model.partita;
 
+
+import java.util.TimerTask;
+import java.util.Timer;
+
 public class Game {
 	
 	private Player player1;
 	private Player player2;
 	
 	private Grid gameGrid;
+
+	//il controller globale passa i giocatori alla partita generata, la partita inizializza la sua grid
+	
+	public Game(Player player1, Player player2) {
+		
+		this.player1 = player1;
+		this.player2 = player2;
+		
+		this.gameGrid = new Grid();
+	}
+	
+	public void giveTurn(Player turnEnder) {
+		//disabilita l'interfaccia per il giocatore che gli passiamo
+	}
+	
 
 }
--- src/model/partita/Player.java
+++ src/model/partita/Player.java
@@ -1,5 +1,7 @@
 package model.partita;
 
 public class Player {
-
+	
+	// variabile flag controllata game per indicare di chi è il turno
+	private boolean isMyTurn;
 }
