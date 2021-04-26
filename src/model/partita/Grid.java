a4378198f68b14a8ef57b7a3d3e392ee37432d08
RobertaM99 <roberta.moro02@universitadipavia.it>
2021-04-26 12:48:21 +0200

    Aggiungo vittoria diagonale

--- src/model/partita/Grid.java
+++ src/model/partita/Grid.java
@@ -1,68 +1,114 @@
 package model.partita;
 
 public class Grid {
-	
-	//Matrice di celle della griglia
-	private Cell[][] gameGrid;
-	private int height = 6;
-	private int lenght = 7;
-	
-	public Grid() {
-		
-		//inizializzazione matrice
-		this.gameGrid = new Cell[height][lenght];
-		
-	}
-	
-	//controllo righe
-	
-	public boolean checkRiga(int x,int y,TokenColor c) {
-		int victoryCounter = 0 ;
-		
-		for(int i=0;i<7;i++) {
-			if(gameGrid[i][y].getTokenColor == c) {
-				victoryCounter++;
-			}
-			else {
-				victoryCounter = 0;
-			}
-		}
-		if(victoryCounter==4) 
-			return true;
-		else
-			return false;
-		
-	}
-	
-	// controllo colonne
-	
-	public boolean checkColonna(int x,int y,TokenColor c) {
-		int victoryCounter = 0 ;
-		
-		for(int i=0;i<7;i++) {
-			if(gameGrid[x][i].getTokenColor == c) {
-				victoryCounter++;
-			}
-			else {
-				victoryCounter = 0;
-			}
-		}
-		if(victoryCounter==4) 
-			return true;
-		else
-			return false;
-	}
-	
-	public boolean checkDiagonale(int x, int y,TokenColor c) {
-		
-		int victoryCounter1 = 0;
-		int victoryCounter2 = 0;
-		
-		for(int i=x,j=y;i>=0||j>=0;i--,j--) {
-			
-		}
-		
-		
-	}
+
+    //Matrice di celle della griglia
+    private Cell[][] gameGrid;
+    private int height = 5;
+    private int lenght = 6;
+
+    public Grid() {
+
+        //inizializzazione matrice
+        this.gameGrid = new Cell[height][lenght];
+
+    }
+
+    //controllo righe
+    public boolean checkRiga(int x,int y,TokenColor c) {
+        int victoryCounter = 0 ;
+
+        for(int i=0;i<6;i++) {
+            if(gameGrid[i][y].getTokenColor == c) {
+                victoryCounter++;
+            }
+            else {
+                victoryCounter = 0;
+            }
+        }
+        if(victoryCounter==4) 
+            return true;
+        else
+            return false;
+
+    }
+
+    // controllo colonne
+
+    public boolean checkColonna(int x,int y,TokenColor c) {
+        int victoryCounter = 0 ;
+
+        for(int i=0;i<6;i++) {
+            if(gameGrid[x][i].getTokenColor == c) {
+                victoryCounter++;
+            }
+            else {
+                victoryCounter = 0;
+            }
+        }
+        if(victoryCounter==4) 
+            return true;
+        else
+            return false;
+    }
+public boolean checkDiagonale(int x, int y,TokenColor c) {
+
+        int victoryCounter1 = 0;
+        int victoryCounter2 = 0;
+
+
+        for(int i=x-1,j=y-1; i<=0 || j<=0; i--,j--) {
+            if(gameGrid[i][j].getTokenColor != c){
+                break;
+            } else victoryCounter1++;
+
+            if(victoryCounter1 == 3){
+
+                return true;
+            }
+
+        }
+
+        for(int i=x+1,j=y+1 ; i>=5 || j>=6 ; i++,j++) {
+            if(gameGrid[i][j].getTokenColor != c){
+                break;
+            } else victoryCounter1++;
+
+            if(victoryCounter1 == 3){
+
+                return true;
+            }
+
+        }
+
+
+        //check other diag (in basso a dx)
+        for(int i=x-1,j=y+1; i<=0 || j<=0; i--,j++) {
+            if(gameGrid[i][j].getTokenColor != c){
+                break;
+            } else victoryCounter2++;
+
+            if(victoryCounter2 == 3){
+                return true;
+            }
+
+        }
+
+        //andiamo in basso a sx
+        for(int i=x+1,j=y-1 ; i>=5||j>=6 ; i++,j--) {
+            if(gameGrid[i][j].getTokenColor != c){
+                break;
+            } else victoryCounter2++;
+
+            if(victoryCounter2 == 3){
+                return true;
+            }
+
+        }
+
+        //il giocatore di turno non ha vinto, via al prossimo ciclo
+        return false;
+
+    }
 
 }
