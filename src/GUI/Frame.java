ab1266b967d677367b7174b7da7511c0cb03ac26
CaterinaGuida <80213159+CaterinaGuida@users.noreply.github.com>
2021-05-13 15:58:28 +0200

    GUI

---
+++ src/GUI/Frame.java
@@ -0,0 +1,55 @@
+package GUI;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JButton;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.SwingConstants;
+import javax.swing.JLabel;
+import java.awt.Toolkit;
+import javax.swing.ImageIcon;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+
+public class Frame extends JFrame {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Frame frame = new Frame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Frame() {
+		setTitle("Forza 4!");
+		setAutoRequestFocus(false);
+		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\catg2\\Desktop\\rosso.png"));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 778, 613);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0};
+		gridBagLayout.rowHeights = new int[]{0};
+		gridBagLayout.columnWeights = new double[]{Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{Double.MIN_VALUE};
+		getContentPane().setLayout(gridBagLayout);
+	}
+
+}
