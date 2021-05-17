338db85dd163fdaf30ec3bdf6a68c8170af09228
FlavioBobba99 <80460277+FlavioBobba99@users.noreply.github.com>
2021-05-17 18:26:22 +0200

    cambio musica

--- src/guiB/Forza4.java
+++ src/guiB/Forza4.java
@@ -51,7 +51,7 @@ public class Forza4 {
 		gameSetup();
 		initialize();
 		this.p = SoundPlayer.getSoundPlayer();
-		p.RiproduciSuono(new File("resources/sounds/01-Radioactive.wav"));
+		p.RiproduciSuono(new File("resources/sounds/Tetris 8 bit Music Tetris Theme Song_2.wav"));
 	}
 
 	/**
