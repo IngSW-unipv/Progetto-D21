82ba3b97c3827616e1bdd9df892b0ae1bb18d421
FlavioBobba99 <80460277+FlavioBobba99@users.noreply.github.com>
2021-06-18 12:59:05 +0200

    bugfix

--- ClientProject/src/menuGUI/mainmenu/InviteFrame.java
+++ ClientProject/src/menuGUI/mainmenu/InviteFrame.java
@@ -49,7 +49,7 @@ public class InviteFrame extends JFrame {
         label1.setForeground(Color.BLACK);
 
         ActionListener sendInvite = e ->{
-            if(this.second_menu!=null&&!second_menu.getGameLenght().equals("a")){
+            if((this.second_menu!=null)&&(!second_menu.getGameLenght().equals("a"))){
                 NetworkThread.getNetworkThread().sendMessage("sendInvite,"+textField.getText()+","+second_menu.getGameLenght());
             }ErrorFrame localErrorFrame = new ErrorFrame("Press Game Mode to set game duration");
             //TODO CHIUDI IL PANNELLO APERTO CORRENTE
